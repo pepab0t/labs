12b4fcf8395297eb60a815160852754b80d2e8a2
pepab0t <cernikpepa@seznam.cz>
2023-09-20 16:35:58 +0200

    initial commit

---
+++ main/api.py
@@ -0,0 +1,107 @@
+from functools import wraps
+from django.http import HttpRequest, JsonResponse
+from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
+from django.db.utils import IntegrityError
+from .models import LabTopic
+import json
+
+
+def unauthorized():
+    return JsonResponse({"message": "unauthorized"}, status=403)
+
+
+def staff_or_403(fn):
+    @wraps(fn)
+    def wrapper(request: HttpRequest, *args, **kwargs):
+        if not request.user.is_staff:  # type: ignore
+            return unauthorized()
+        return fn(request, *args, **kwargs)
+
+    return wrapper
+
+
+def handle_validation(fn):
+    @wraps(fn)
+    def wrapper(request: HttpRequest, *args, **kwargs):
+        try:
+            return fn(request, *args, **kwargs)
+        except ValidationError as e:
+            return JsonResponse(
+                {"message": e.message, "error": "validation"}, status=422
+            )
+
+    return wrapper
+
+
+@staff_or_403
+@handle_validation
+def new_topic(request: HttpRequest):
+    if request.method != "POST":
+        return unauthorized()
+
+    topic: str | None = json.loads(request.body.decode("utf-8")).get("topic")
+    if topic is None:
+        return JsonResponse({"message": "missing parameter `topic`"}, status=422)
+
+    lab_topic = LabTopic(title=topic, created_by=request.user)
+    lab_topic.full_clean()
+    try:
+        lab_topic.save()
+    except:
+        return JsonResponse({"message": "topic already exist"}, status=422)
+
+    lab_topic.refresh_from_db()
+    return JsonResponse(lab_topic.json(), status=201)
+
+
+def all_topics(request: HttpRequest):
+    if request.method != "GET":
+        return unauthorized()
+
+    topics = LabTopic.objects.all()
+    return JsonResponse([t.json() for t in topics], status=200, safe=False)
+
+
+@staff_or_403
+def remove_topic(request: HttpRequest):
+    if request.method != "DELETE":
+        return unauthorized()
+
+    id: int | None = json.loads(request.body.decode("utf-8")).get("id")
+    if id is None:
+        return JsonResponse({"message": "missing parameter `topic`"}, status=422)
+
+    try:
+        LabTopic.objects.get(pk=id).delete()
+    except LabTopic.DoesNotExist:
+        return JsonResponse({"message": f"topic {id} does not exist"}, status=400)
+
+    return JsonResponse({}, status=204)
+
+
+@staff_or_403
+def modify_topic(request: HttpRequest):
+    if request.method != "PUT":
+        return unauthorized()
+
+    body = json.loads(request.body.decode("utf-8"))
+
+    if (id := body.get("id")) is None or (new_title := body.get("topic")) is None:
+        return JsonResponse(
+            {"message": "payload expected to have `id` and `topic`"}, status=400
+        )
+
+    lab_topic = get_object_or_404(LabTopic, pk=id)
+
+    lab_topic.title = new_title
+    lab_topic.full_clean()
+    try:
+        lab_topic.save()
+    except IntegrityError:
+        return JsonResponse(
+            {"message": f"Topic `{new_title}` already exists"}, status=422
+        )
+
+    lab_topic.refresh_from_db()
+    return JsonResponse(lab_topic.json(), status=200)
---
+++ main/models.py
@@ -0,0 +1,123 @@
+from collections.abc import Iterable
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator, EmailValidator
+
+from datetime import timedelta
+
+
+def more_than_one_word(value: str):
+    if len(value.strip().split()) == 0:
+        raise ValidationError("Téma musí obsahovat alespoň jedno slovo", code="invalid")
+
+
+# Create your models here.
+class LabTopic(models.Model):
+    title = models.TextField(
+        max_length=200, blank=False, validators=[more_than_one_word], unique=True
+    )
+    created_by = models.ForeignKey(
+        "CustomUser", related_name="topics_created", on_delete=models.CASCADE
+    )
+
+    def json(self):
+        return {"id": self.id, "title": self.title, "created_by": self.created_by.email}  # type: ignore
+
+
+class LabEvent(models.Model):
+    datetime = models.DateTimeField(null=False, blank=False)
+    close_login = models.DateTimeField(null=True, blank=False)
+    close_logout = models.DateTimeField(null=True, blank=False)
+
+    created_by = models.ForeignKey(
+        "CustomUser", related_name="events_created", on_delete=models.CASCADE
+    )
+
+    def save(self, *args, **kwargs) -> None:
+        if self.datetime < self.datetime.now():
+            raise ValidationError("Date of Lab cannot be in the past", code="invalid")
+
+        if self.close_login is None:
+            raise ValidationError("Close login date must be specified", code="invalid")
+        if self.close_logout is None:
+            raise ValidationError("Close logout date must be specified", code="invalid")
+
+        if self.close_login > self.datetime:
+            raise ValidationError(
+                "Lab log in closing date cannot be greater than lab date",
+                code="invalid",
+            )
+
+        if self.close_logout > self.datetime:
+            raise ValidationError(
+                "Lab log out closing date cannot be greater than lab date",
+                code="invalid",
+            )
+
+        super().save(*args, **kwargs)
+
+
+class TopicCapacity:
+    topic = models.ForeignKey(LabTopic, on_delete=models.CASCADE, related_name="labs")
+    event = models.ForeignKey(LabEvent, on_delete=models.CASCADE, related_name="labs")
+    capacity = models.PositiveIntegerField(
+        validators=(MinValueValidator(1), MaxValueValidator(1_000))
+    )
+
+    users = models.ManyToManyField("CustomUser", related_name="labs")
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    id = models.BigAutoField(primary_key=True)
+    username = None
+    first_name = None
+    last_name = None
+
+    email = models.EmailField(
+        unique=True,
+    )
+    fullname = models.CharField(validators=[MinValueValidator(4)], max_length=55)
+
+    # labs = models.ManyToManyField(Lab, related_name="students")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = UserManager()
+
+    def json(self):
+        return {"fullname": self.fullname, "email": self.email}
