bc4de7286025322e88b0332d1c93d254ab682d9e
Patricie <patricie.petrikova@isobar.com>
2023-10-08 15:46:06 +0200

    responsivity add

--- .history/main/admin_20231006095744.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, LabEvent, LabTopic, LinkTopicEvent
-
-# Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(LabEvent)
-admin.site.register(LabTopic)
-admin.site.register(LinkTopicEvent)
